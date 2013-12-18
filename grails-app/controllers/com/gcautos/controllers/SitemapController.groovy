57d42f77d81501fcc58609614a4fb5abb9a6deec
Julien ROTT <julien.rott@gmail.com>
2013-12-18 21:57:50 +0100

    add sitemap controller

---
+++ grails-app/controllers/com/gcautos/controllers/SitemapController.groovy
@@ -0,0 +1,37 @@
+package com.gcautos.controllers
+
+class SitemapController {
+
+    def index() {
+		render(contentType: 'text/xml', encoding: 'UTF-8') {
+			mkp.yieldUnescaped '<?xml version="1.0" encoding="UTF-8"?>'
+			urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+					'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+					'xsi:schemaLocation': "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd") {
+				url {
+					loc("http://www.gcautos.fr")
+					changefreq('weekly')
+					priority(1.0)
+				}
+				url {
+					loc("ttp://www.gcautos.fr/voitures/occasions/")
+				}
+				url {
+					loc("ttp://www.gcautos.fr/voitures/neuves/")
+				}
+				url {
+					loc("ttp://www.gcautos.fr/voitures/quads/")
+				}
+				url {
+					loc("ttp://www.gcautos.fr/voitures/dirts/")
+				}
+				url {
+					loc("ttp://www.gcautos.fr/voitures/electriques/")
+				}
+				url {
+					loc("ttp://www.gcautos.fr/contact/index")
+				}
+			}
+	   }
+	}
+}
---
+++ test/unit/com/gcautos/controllers/SitemapControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.gcautos.controllers
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(SitemapController)
+class SitemapControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
